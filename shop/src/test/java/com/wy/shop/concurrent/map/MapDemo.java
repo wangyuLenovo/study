f634b173dc53f05fbf690d84d245e70ff7eed79d
wangyu888888 <13093665220@163.com>
2019-04-19 17:03:49 +0800

    测试git

---
+++ shop/src/test/java/com/wy/shop/concurrent/map/MapDemo.java
@@ -0,0 +1,31 @@
+package com.wy.shop.concurrent.map;
+
+import com.wy.shop.util.WriterHelper;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * describe:
+ *
+ * @author wangyu
+ * @date 2019/4/19
+ * @time 16:56
+ */
+public class MapDemo {
+
+
+    public static void main(String[] args) {
+
+
+        ConcurrentHashMap<Object, Object> objectObjectConcurrentHashMap = new ConcurrentHashMap<>();
+        for (int i = 0; i < 100; i++) {
+            objectObjectConcurrentHashMap.put("WY"+i,"哈哈哈"+i);
+        }
+        WriterHelper.writeObjInfo(objectObjectConcurrentHashMap);
+    }
+
+
+
+
+
+}
